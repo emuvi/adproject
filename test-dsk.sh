dc7505a3cf1986bcd328919d20bef57e63bd99f9
Éverton M. Vieira <everton.muvi@gmail.com>
2022-01-19 18:18:27 -0300

    normalization qinpel apps

--- test-dsk.sh
+++ test-dsk.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 bash -v build.sh
 browserify build/index.js --debug -o public/index.js
 rm -rf ../qinpel-dsk/run/app/adproject
 mkdir ../qinpel-dsk/run/app/adproject
 cd public
-cp -r * ../../qinpel-dsk/run/app/adproject/
+cp -r * ../../qinpel-dsk/run/app/adproject
 cd ..
